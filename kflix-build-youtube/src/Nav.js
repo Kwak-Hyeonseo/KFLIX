d8502fcf4ce11649b3375bfded85a0b2316e23a4
KWAK HYEONSEO <kwak980319@gmail.com>
2022-11-29 14:42:38 +0900

    Setup draft (Nav/Banner)

--- kflix-build-youtube/src/Nav.js
+++ kflix-build-youtube/src/Nav.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Nav.css"
 
 
 function Nav() {
+const [show, handleShow] = useState(false)
+const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+        handleShow(true);
+    }
+    else {
+        handleShow(false)
+    }
+}
+
+useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+}, [])
+
     return (
-        <div className="nav">
+        // show가 ture일 때만 nav__black 활성화
+        <div className={`nav ${show && "nav__black"}`}>
             <div className="nav__contents">
                 <img className="nav__logo"
-                    src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
+                    src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                     alt=""
                 />
                 <img className="nav__avatar"
