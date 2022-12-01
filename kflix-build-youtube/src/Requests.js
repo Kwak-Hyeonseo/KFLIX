0f5cc53d44f0374ae70045c4b0cf0736f06ba60f
KWAK HYEONSEO <kwak980319@gmail.com>
2022-12-01 16:47:49 +0900

    Request TMDB data for Banner

--- kflix-build-youtube/src/Requests.js
+++ kflix-build-youtube/src/Requests.js
@@ -3,7 +3,7 @@ const API_KEY = "662e6b633afa2b84779ac71bdaa40947";
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginas: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
--- kflix-build-youtube/src/axios.js
+++ kflix-build-youtube/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const instance = axios.creat({
+const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
 });
 
