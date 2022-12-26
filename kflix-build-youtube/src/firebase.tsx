7b252b1848fb4f06b304fc66d817ee33453a341d
KWAK HYEONSEO <kwak980319@gmail.com>
2022-12-26 14:08:53 +0900

    Modify firebase.tsx

--- kflix-build-youtube/src/firebase.tsx
+++ kflix-build-youtube/src/firebase.tsx
@@ -2,18 +2,21 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-const firebaseConfig = {
+
+type Config = { apiKey: string, authDomain: string, projectId: string, storageBucket: string, messagingSenderId: string, appId: string}
+
+const firebaseConfig: Config = {
   apiKey: "AIzaSyBSIeVo6NILlmSILhglGeWgcifbGFl-lJM",
   authDomain: "netflix-clone-81eea.firebaseapp.com",
   projectId: "netflix-clone-81eea",
   storageBucket: "netflix-clone-81eea.appspot.com",
   messagingSenderId: "828355564808",
   appId: "1:828355564808:web:9c42c122acc5d4cea408bd",
-};
+}
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
 
-export { auth };
-export default db;
+export { auth }
+export default db
