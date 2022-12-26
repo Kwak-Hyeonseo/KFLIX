ae1793a82ccf17c0270afa3f9c01d3342c3bf3e4
KWAK HYEONSEO <kwak980319@gmail.com>
2022-12-26 11:30:44 +0900

    Modify Requests.tsx

--- kflix-build-youtube/src/Requests.tsx
+++ kflix-build-youtube/src/Requests.tsx
@@ -1,14 +1,16 @@
-// Typically we would store in {process.env.API_KEY}
-const API_KEY = "662e6b633afa2b84779ac71bdaa40947";
+type Fetch = {fetchTrending: string, fetchNetflixOriginals: string, fetchTopRated: string, fetchActionMovies: string, fetchComedyMovies: string, fetchHorrorMovies: string, fetchRomanceMovies: string, fetchDocumentaries: string}
 
-const requests = {
+// Typically we would store in {process.env.API_KEY}
+const API_KEY: string = "662e6b633afa2b84779ac71bdaa40947";
+
+const requests: Fetch = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovie: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 
